bcf40425f755c568d1b371b857ad032af65a3335
Cyrille <jibacyrille@gmail>
2024-08-03 08:47:57 -0400

    premier commit

---
+++ frontend-ang-v1/src/app/app-routing.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LoadStudentsComponent } from './load-students/load-students.component';
+import { LoadPaymentsComponent } from './load-payments/load-payments.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { StudentsComponent } from './students/students.component';
+import { PaymentsComponent } from './payments/payments.component';
+import { AdminTemplateComponent } from './admin-template/admin-template.component';
+import { AuthGuard } from './guards/auth.guard';
+
+const routes: Routes = [
+  {path: "", component : LoginComponent},
+  {path: "login", component : LoginComponent},
+  {path: "admin", component : AdminTemplateComponent, 
+    canActivate : [AuthGuard],
+    children : [
+      {path: "home", component : HomeComponent},
+      {path: "profile", component : ProfileComponent},
+      {path: "loadStudents", component : LoadStudentsComponent},
+      {path: "loadPayments", component : LoadPaymentsComponent},
+      {path: "dashboard", component : DashboardComponent},
+      {path: "students", component : StudentsComponent},
+      {path: "payments", component : PaymentsComponent}
+    ]},
+  
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
